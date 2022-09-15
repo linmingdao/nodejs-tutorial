2d205dde1cd55d4a9b8c4467d6382229b074824b
Linmingdao <mr.lmd@outlook.com>
2022-09-15 10:21:03 +0800

    update: 移除不必要的代码

--- src/P74/client_resolve_stick.js
+++ src/P74/client_resolve_stick.js
@@ -8,12 +8,9 @@ client.on('connect', () => {
   client.write('中国');
   // 通过延时发送解决TCP粘包问题 -> 当然这样做降低了TCP的传输效率，如何拆包、封包后续会涉及
   for (let i = 0; i < dataArr.length; i++) {
-    // (function (i) {
-    // 已经是闭包了，没必要再使用闭包包一层了
     setTimeout(() => {
       client.write(dataArr[i]);
     }, 1000 * (i + 1));
-    // })(i);
   }
 });
 
