0b0a2eae5a0612933b4c822d0c2e465e3c4437d5
Linmingdao <mr.lmd@outlook.com>
2022-10-13 16:40:37 +0800

    feat: cache-control实现强制缓存

--- src/http-cache/P2_02_cache_control/app.js
+++ src/http-cache/P2_02_cache_control/app.js
@@ -9,12 +9,20 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-type', 'text/html;charset=utf-8');
     res.end(data);
   } else if (pathname === '/img/01.png') {
-    console.log('请求进来了');
+    console.log('01.png的请求进来了');
     const data = fs.readFileSync('./img/01.png');
-    // 强制缓存设置的响应头（http 1.0 就已支持）
+    // 强制缓存设置的响应头【expires】（http 1.0 就已支持）
     res.writeHead(200, {
       Expires: new Date('2022-10-13 13:39:59').toUTCString(),
     });
+    res.end(data);
+  } else if (pathname === '/img/02.png') {
+    console.log('02.png的请求进来了');
+    const data = fs.readFileSync('./img/02.png');
+    // 强制缓存设置的响应头【cache-control】（http 1.1 支持）：
+    // cache-control采用滑动时间（既从响应给客户端多少秒后缓存失效），单位是秒
+    // 解决 expires 过分依赖本地时间戳带来的缓存问题
+    res.writeHead(200, { 'Cache-Control': 'max-age=5' });
     res.end(data);
   } else {
     res.statusCode = 404;
